e1ba76573dab02e9ef4fbdffd1010b8561cba87f
Jorge Alberto Schelotto <jasfotografo@hotmail.com>
2019-09-08 22:46:44 -0300

    Lista galeria

--- scripts/validateContact.js
+++ scripts/validateContact.js
@@ -1,3 +1,52 @@
+$(document).ready(function() {
+  $("body").fadeIn(2000);
+  $("img").fadeIn(2000);
+  $(".filter-button").click(function() {
+    var value = $(this).attr("data-filter");
+
+    if (value == "all") {
+      //$('.filter').removeClass('hidden');
+      $(".filter").show("1000");
+    } else {
+      //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+      //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+      $(".filter")
+        .not("." + value)
+        .hide("3000");
+      $(".filter")
+        .filter("." + value)
+        .show("3000");
+    }
+  });
+  if ($(".filter-button").removeClass("active")) {
+    $(this).removeClass("active");
+  }
+  $(this).addClass("active");
+  $(".ir-arriba").click(function() {
+    $("body, html").animate(
+      {
+        scrollTop: "0px"
+      },
+      300
+    );
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $(".ir-arriba").slideDown(300);
+    } else {
+      $(".ir-arriba").slideUp(300);
+    }
+  });
+});
+
+$(function() {
+  var foo = $("#gallery");
+  foo.poptrox({
+    usePopupCaption: true
+  });
+});
+
 function validateForm() {
   var name = document.getElementById("name").value;
   if (name == "") {
@@ -27,3 +76,96 @@ function validateForm() {
   document.getElementById("status").innerHTML = "Enviando...";
   document.getElementById("contact-form").submit();
 }
+
+$(function() {
+  var selectedClass = "";
+  $(".filter").click(function() {
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div")
+      .not("." + selectedClass)
+      .fadeOut()
+      .removeClass("animation");
+    setTimeout(function() {
+      $("." + selectedClass)
+        .fadeIn()
+        .addClass("animation");
+      $("#gallery").fadeTo(300, 1);
+    }, 300);
+  });
+});
+filterSelection("all"); // Execute the function and show all columns
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+$(window).load(function() {
+  var $container = $(".portfolioContainer");
+  $container.isotope({
+    filter: "*",
+    animationOptions: {
+      duration: 750,
+      easing: "linear",
+      queue: false
+    }
+  });
+
+  $(".portfolioFilter a").click(function() {
+    $(".portfolioFilter .current").removeClass("current");
+    $(this).addClass("current");
+
+    var selector = $(this).attr("data-filter");
+    $container.isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 750,
+        easing: "linear",
+        queue: false
+      }
+    });
+    return false;
+  });
+});
