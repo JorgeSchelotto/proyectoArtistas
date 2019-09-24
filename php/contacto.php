c88db3d52e3b6e733fe36ce49ad4e40a873b5bbb
Jorge Alberto Schelotto <jasfotografo@hotmail.com>
2019-09-23 21:11:58 -0300

    Maquetado ok, esperando correcciones

--- php/contacto.php
+++ php/contacto.php
@@ -3,14 +3,13 @@ if(isset( $_POST['name']))
 $name = $_POST['name'];
 if(isset( $_POST['email']))
 $email = $_POST['email'];
-if(isset( $_POST['message']))
-$message = $_POST['message'];
 if(isset( $_POST['subject']))
 $subject = 'Página Guada Gárriz';
+$message = 'Deseo que me avisen cuando este lista la página.'
 
 $content="From: $name \n Email: $email \n Message: $message";
-$recipient = "jasfotografo@hotmail.com";
+$recipient = "gabrielavictoriaw@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
-echo "Email sent!";
+header ("Location: http://guadagarriz.com.ar");
 ?>
