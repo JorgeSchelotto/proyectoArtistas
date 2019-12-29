b0b80d0c01d46b4c25b85e0101574c954be3ef72
Jorge Alberto Schelotto <jasfotografo@hotmail.com>
2019-12-29 12:14:58 -0300

    Correcciones 1, parte E

--- scripts/validateContact.js
+++ scripts/validateContact.js
@@ -133,8 +133,8 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+// var btnContainer = document.getElementById("myBtnContainer");
+var btns = document.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
