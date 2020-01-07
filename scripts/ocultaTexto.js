57628e047d57a4c12e38faf1998449d3a9839cfb
Jorge Alberto Schelotto <jasfotografo@hotmail.com>
2020-01-07 01:08:25 -0300

    correcion 1 paso: G

--- scripts/ocultaTexto.js
+++ scripts/ocultaTexto.js
@@ -1,19 +1,52 @@
+// Nodos
 var infancias = document.getElementById("textoInfancias");
-var niño = document.getElementById("niño");
-var todas = document.getElementById("todas");
+var vidas = document.getElementById("textoVidas");
+
+//BTN
+var btnNiño = document.getElementById("niño");
+var btnTodas = document.getElementById("todas");
+var btnVidas = document.getElementById("vidas");
 
 infancias.setAttribute("hidden", true);
-// Oculta texto al cargar
-document.addEventListener("load", () => {
-  infancias.setAttribute("hidden", true);
-});
+vidas.setAttribute("hidden", true);
 
 // Muestra texto
-niño.addEventListener("click", () => {
+btnNiño.addEventListener("click", () => {
   infancias.removeAttribute("hidden");
+  $(".leer").hide(200);
+  $(".leerMas").show();
+  $(".mostrarMenos").hide();
+});
+
+// Oculta texto al clikear boton
+btnTodas.addEventListener("click", () => {
+  infancias.setAttribute("hidden", true);
+  vidas.setAttribute("hidden", true);
+  $(".leer").hide(200);
+  $(".leerMas").show();
+  $(".mostrarMenos").hide();
 });
 
 // Mustra texto al clikear boton
-todas.addEventListener("click", () => {
-  infancias.setAttribute("hidden", true);
+btnVidas.addEventListener("click", () => {
+  vidas.removeAttribute("hidden");
+  $(".leer").hide(200);
+  $(".leerMas").show();
+  $(".mostrarMenos").hide();
+});
+
+//Oculta texto
+
+$(".leer").fadeToggle(200);
+
+$(".leerMas").click(() => {
+  $(".leer").fadeToggle(200);
+  $(".leerMas").hide();
+  $(".mostrarMenos").show();
+});
+
+$(".mostrarMenos").click(() => {
+  $(".leer").fadeToggle(200);
+  $(".leerMas").show();
+  $(".mostrarMenos").hide();
 });
