c64b72bd7232c2d102c84730c3df272019504d24
Jorge Alberto Schelotto <jasfotografo@hotmail.com>
2020-03-04 01:02:00 -0300

    denuevo

--- php/contacto.php
+++ php/contacto.php
@@ -8,7 +8,7 @@ $subject = 'Página Guada Gárriz';
 $message = 'Deseo que me avisen cuando este lista la página.'
 
 $content="From: $name \n Email: $email \n Message: $message";
-$recipient = "gabrielavictoriaw@gmail.com";
+$recipient = "contacto@guadagarriz.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 header ("Location: http://guadagarriz.com.ar");
